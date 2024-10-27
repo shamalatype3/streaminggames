43328c607e28624a9e2b8396d373539e34349140
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-10-27 08:59:02 +0000

    Updating to latest FlutterFlow output.

--- lib/login_sign_up/onboarding/onboarding_widget.dart
+++ lib/login_sign_up/onboarding/onboarding_widget.dart
@@ -36,138 +36,114 @@ class _OnboardingWidgetState extends State<OnboardingWidget> {
       onTap: () => FocusScope.of(context).unfocus(),
       child: Scaffold(
         key: scaffoldKey,
-        backgroundColor: FlutterFlowTheme.of(context).primaryBackground,
-        body: Container(
-          width: MediaQuery.sizeOf(context).width * 1.0,
-          decoration: BoxDecoration(
-            color: FlutterFlowTheme.of(context).info,
-          ),
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              Container(
-                decoration: const BoxDecoration(
-                  color: Color(0x00525252),
+        backgroundColor: FlutterFlowTheme.of(context).info,
+        body: SafeArea(
+          top: true,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    'assets/images/fondo_login_2.jpg',
+                    width: MediaQuery.sizeOf(context).width * 1.0,
+                    fit: BoxFit.cover,
+                  ),
                 ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(8.0),
-                      child: Image.asset(
-                        'assets/images/fondo_login_2.jpg',
-                        width: MediaQuery.sizeOf(context).width * 1.0,
-                        fit: BoxFit.cover,
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 0.0),
-                      child: Text(
-                        'KEEP AN EYE ON THE STADIUM',
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'Montserrat',
-                              color: FlutterFlowTheme.of(context).secondary,
-                              fontSize: 56.0,
-                              letterSpacing: 0.0,
-                              fontWeight: FontWeight.w900,
-                            ),
-                      ),
-                    ),
-                    Padding(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 0.0),
-                      child: Text(
-                        'Watch your favorite tennis matches from across the country live from your device',
-                        textAlign: TextAlign.start,
-                        style: FlutterFlowTheme.of(context).bodyMedium.override(
-                              fontFamily: 'Roboto Slab',
-                              color: FlutterFlowTheme.of(context).secondary,
-                              fontSize: 16.0,
-                              letterSpacing: 0.0,
-                            ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              Container(
-                decoration: const BoxDecoration(
-                  color: Color(0x00525252),
-                ),
-                child: Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Padding(
-                      padding:
-                          const EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 10.0),
-                      child: FFButtonWidget(
-                        onPressed: () async {
-                          context.pushNamed('Login');
-                        },
-                        text: 'login',
-                        options: FFButtonOptions(
-                          width: double.infinity,
-                          height: 40.0,
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              24.0, 0.0, 24.0, 0.0),
-                          iconPadding: const EdgeInsetsDirectional.fromSTEB(
-                              0.0, 0.0, 0.0, 0.0),
-                          color: FlutterFlowTheme.of(context).primary,
-                          textStyle:
-                              FlutterFlowTheme.of(context).titleSmall.override(
-                                    fontFamily: 'Bebas Neue',
-                                    color: Colors.white,
-                                    fontSize: 20.0,
-                                    letterSpacing: 0.0,
-                                  ),
-                          elevation: 0.0,
-                          borderSide: const BorderSide(
-                            color: Colors.transparent,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(8.0),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 0.0),
+                  child: Text(
+                    'KEEP AN EYE ON THE GAME',
+                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                          fontFamily: 'Montserrat',
+                          color: FlutterFlowTheme.of(context).secondary,
+                          fontSize: 38.0,
+                          letterSpacing: 0.0,
+                          fontWeight: FontWeight.w900,
                         ),
-                      ),
-                    ),
-                    Padding(
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 0.0),
+                  child: Text(
+                    'Watch your favorite tennis matches from across the country live from your device',
+                    textAlign: TextAlign.start,
+                    style: FlutterFlowTheme.of(context).bodyMedium.override(
+                          fontFamily: 'Roboto Slab',
+                          color: FlutterFlowTheme.of(context).secondary,
+                          fontSize: 16.0,
+                          letterSpacing: 0.0,
+                        ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 10.0),
+                  child: FFButtonWidget(
+                    onPressed: () async {
+                      context.pushNamed('Login');
+                    },
+                    text: 'login',
+                    options: FFButtonOptions(
+                      width: double.infinity,
+                      height: 40.0,
                       padding:
-                          const EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 20.0),
-                      child: FFButtonWidget(
-                        onPressed: () async {
-                          context.pushNamed('CreateAccount');
-                        },
-                        text: 'CREATE ACCOUNT',
-                        options: FFButtonOptions(
-                          width: double.infinity,
-                          height: 40.0,
-                          padding: const EdgeInsetsDirectional.fromSTEB(
-                              24.0, 0.0, 24.0, 0.0),
-                          iconPadding: const EdgeInsetsDirectional.fromSTEB(
-                              0.0, 0.0, 0.0, 0.0),
-                          color: const Color(0x1F000000),
-                          textStyle: FlutterFlowTheme.of(context)
-                              .titleSmall
-                              .override(
+                          const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                      iconPadding:
+                          const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                      color: FlutterFlowTheme.of(context).primary,
+                      textStyle:
+                          FlutterFlowTheme.of(context).titleSmall.override(
+                                fontFamily: 'Bebas Neue',
+                                color: Colors.white,
+                                fontSize: 20.0,
+                                letterSpacing: 0.0,
+                              ),
+                      elevation: 0.0,
+                      borderSide: const BorderSide(
+                        color: Colors.transparent,
+                        width: 1.0,
+                      ),
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsetsDirectional.fromSTEB(10.0, 0.0, 10.0, 20.0),
+                  child: FFButtonWidget(
+                    onPressed: () async {
+                      context.pushNamed('CreateAccount');
+                    },
+                    text: 'CREATE ACCOUNT',
+                    options: FFButtonOptions(
+                      width: double.infinity,
+                      height: 40.0,
+                      padding:
+                          const EdgeInsetsDirectional.fromSTEB(24.0, 0.0, 24.0, 0.0),
+                      iconPadding:
+                          const EdgeInsetsDirectional.fromSTEB(0.0, 0.0, 0.0, 0.0),
+                      color: const Color(0x1F000000),
+                      textStyle:
+                          FlutterFlowTheme.of(context).titleSmall.override(
                                 fontFamily: 'Bebas Neue',
                                 color: FlutterFlowTheme.of(context).secondary,
                                 fontSize: 20.0,
                                 letterSpacing: 0.0,
                               ),
-                          elevation: 0.0,
-                          borderSide: const BorderSide(
-                            color: Colors.transparent,
-                            width: 1.0,
-                          ),
-                          borderRadius: BorderRadius.circular(8.0),
-                        ),
+                      elevation: 0.0,
+                      borderSide: const BorderSide(
+                        color: Colors.transparent,
+                        width: 1.0,
                       ),
+                      borderRadius: BorderRadius.circular(8.0),
                     ),
-                  ],
+                  ),
                 ),
-              ),
-            ],
+              ].divide(const SizedBox(height: 14.0)),
+            ),
           ),
         ),
       ),
